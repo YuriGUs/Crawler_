74efe5f7dcea9e47542ce3242cff2dd092b0574f
YuriGUs <gustavo000y@gmail.com>
2023-09-27 23:10:05 -0300

    finalized

---
+++ crawler/src/index.ts
@@ -0,0 +1,16 @@
+import { CrawlerCorinthiansController } from 'controller/crawler-corinthians.controller';
+import { CrawlerPalmeirasController } from 'controller/crawler-palmeiras.controller';
+
+class Init {
+  constructor() {
+    this._init();
+  }
+
+  private _init() {
+    new CrawlerPalmeirasController().init();
+    new CrawlerCorinthiansController().init();
+    console.log('Inicializado...');
+  }
+}
+
+new Init();
---
+++ crawler/src/service/start-puppeteer.service.ts
@@ -0,0 +1,47 @@
+import fs from 'node:fs';
+import { IFileGenerator } from 'interface/file-generator.interface';
+import puppeteer, { Page } from 'puppeteer';
+import path from 'node:path';
+
+class StartPuppeteerService {
+  constructor() {}
+
+  public start(url: string): Promise<Page> {
+    return new Promise(async (resolve, reject) => {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      const allPages = await browser.pages();
+      allPages[0].close();
+
+      await page.goto(url);
+
+      if (!page) return reject('Configuração não corresponde');
+
+      return resolve(page);
+    });
+  }
+
+  public fileGenerator(payload: Array<IFileGenerator>, fileName: string) {
+    const pathTmp = 'tmp';
+
+    if (!fs.existsSync(path.resolve(pathTmp))) {
+      fs.mkdirSync(path.resolve(pathTmp));
+      console.log('pasta criada com sucesso');
+    }
+
+    const csvRows = payload.map((res: IFileGenerator) => {
+      return `${res.link};${res.titulo};${res.data}`;
+    });
+
+    const csvContent = `Link;Titulo;Data da Postagem\n${csvRows.join('\n')}`;
+
+    try {
+      fs.writeFileSync(`${path.resolve(pathTmp, fileName)}.csv`, csvContent);
+      return console.log('Arquivo criado com sucesso');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export const startPuppeteerService = new StartPuppeteerService();
